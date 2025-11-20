2bea315b8150e76ee397e3de97655f43fcbaf9ca
Kra1nne <cabarrubias2003@gmail.com>
2025-11-21 02:04:02 +0800

    adjusting the domain

--- frontend/src/components/request/UserRequest.jsx
+++ frontend/src/components/request/UserRequest.jsx
@@ -39,7 +39,7 @@ export default function UserRequest() {
                             type="text"
                             value={searchTerm}
                             onChange={(e) => setSearchTerm(e.target.value)}
-                            placeholder="Enter soil name..."
+                            placeholder="Enter the name..."
                             className="h-10 sm:h-11 w-full rounded-lg px-3 sm:px-4 border border-gray-300 dark:border-gray-600 sm:max-w-none xl:w-[430px] dark:bg-gray-800 dark:text-white focus:ring-2 focus:ring-success-500 focus:border-transparent placeholder:text-gray-500 text-sm sm:text-base"
                         />
                     </div>
