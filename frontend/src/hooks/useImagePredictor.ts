88367b39b897f2c0ece380095db67bd02d64e9ab
Kra1nne <cabarrubias2003@gmail.com>
2025-11-21 19:12:56 +0800

    adding a model URL

--- frontend/src/hooks/useImagePredictor.ts
+++ frontend/src/hooks/useImagePredictor.ts
@@ -58,7 +58,7 @@ export function useImagePredictor() {
 
   const { model, loading: modelLoading } = useModel();
 
-  const MODEL_SERVER_URL = "https://soilsnapmodel.up.railway.app/predict";
+  const MODEL_SERVER_URL = "https://soilsnap-model.up.railway.app/predict";
   const useServerWhenOnline = process.env.REACT_APP_USE_SERVER_PREDICTION !== "false";
 
   const fileToTensor = async (file: File) => {
